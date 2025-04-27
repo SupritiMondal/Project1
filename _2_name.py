b02f51458c0d037719be5931a0cef1742246499a
SupritiMondal <88269804+SupritiMondal@users.noreply.github.com>
2025-04-27 11:38:17 +0530

    Add files via upload

---
+++ _2_name.py
@@ -0,0 +1,84 @@
+'''
+name    :   If we are having name attribute, we can go for name locator
+
+'''
+import time
+
+## Eg1
+
+# from selenium import webdriver
+# driver = webdriver.Firefox()
+#
+# driver.get('https://www.instagram.com/')
+# time.sleep(2)
+# driver.find_element('name', 'username').send_keys('Yuvraj Singh')
+# time.sleep(1)
+# driver.find_element('name', 'password').send_keys('yuvraj@12345')
+#
+
+####################################################################
+# ## Eg2
+#
+# from selenium import webdriver
+# driver = webdriver.Firefox()
+#
+# driver.get('https://www.saucedemo.com/')
+# time.sleep(2)
+#
+# driver.find_element('name', 'user-name').send_keys('standard_user')
+# time.sleep(1)
+# driver.find_element('name', 'password').send_keys('secret_sauce')
+# time.sleep(1)
+# driver.find_element('name', 'login-button').click()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
